30959f8a83edafac0b228e82a36b7593e11a4f14
Seth Etter <sethetter@gmail.com>
2013-10-12 01:24:56 -0500

    I'm an idiot.. it works in the browser now too
    I was doing an ng-repeat on catalog, not catalog.items. I also update
    the test JSON file for a broken URL.

--- app/scripts/controllers/catalog.js
+++ app/scripts/controllers/catalog.js
@@ -1,19 +1,18 @@
 'use strict';
 
 angular.module( 'catalogApp' )
-  .controller( 'CatalogCtrl', [ '$scope', 'Session', '$location', 'Catalogs',
-  function( $scope, Session, $location, Catalogs ) {
+  .controller( 'CatalogCtrl', [ '$scope', '$timeout', 'Session', '$location', 'Catalogs',
+  function( $scope, $timeout, Session, $location, Catalogs ) {
 
-    //if (!Session.user) $location.path('/login');
+    if (!Session.user) $location.path('/login');
+
     Session.user = {
       "defaultCatalog": 0
     };
 
     Catalogs.find( Session.user.defaultCatalog ).then( function( catalog ) {
-
       $scope.catalog = catalog;
-
-    } );
+    } );;
 
   }
 ] );
